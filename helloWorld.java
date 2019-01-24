b6d5cfabd9e9f19cce01716027f57efc9befb821
bhaskar2 <ibhaskar.ch@gmail.com>
2019-01-24 12:38:40 +0000

    this is first commit

---
+++ helloWorld.java
@@ -0,0 +1,16 @@
+class Hello {
+  int num1= 10;
+  int num2=100;
+  void add(){
+     sum number = num1+num2;
+     System.out.println("sum of two number",number);
+  }
+
+ public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+
+ 
+System.out.println("new change");
+
+}
